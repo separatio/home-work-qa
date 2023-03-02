ccf17f4ccb37fbe25eefa4a069f20d1539afc6fd
Alex Radulescu <allex.radulescu91@gmail.com>
2023-03-02 16:20:43 +0100

    feat: add all findPetByTag test cases

--- Assumptions.md
+++ Assumptions.md
@@ -1,1 +1,6 @@
 <!-- Add any assumptions here -->
+
+## findPetByTag
+
+- when tags such as [tag1, tag2] exist, it is expected to return the same object twice
+- when no pets are found, the status code returned is 200
--- cypress/e2e/findPetByTag.cy.ts
+++ cypress/e2e/findPetByTag.cy.ts
@@ -2,44 +2,49 @@
 const tagsUrl = 'api/v3/pet/findByTags?'
 
 describe('GET /pet/findByTags', () => {
-  it('lists pets tagged with tag1', () => {
+  it('supports one existing tag', () => {
     cy.request(`${tagsUrl}tags=tag1`).as('taggedPets')
 
     cy.fixture('findPetByTag/tag1.json').then((json) => {
       cy.get('@taggedPets').then((response: any) => {
-        expect(response.body).to.deep.equal(json)
         expect(response.status).to.eq(200)
+
+        expect(response.body).to.deep.equal(json)
       })
     })
   })
 
-  // it('lists pets tagged with tag1 and tag2', () => {
-  //   cy.request(`${tagsUrl}tags=tag1&tags=tag2`).as('taggedPets')
+  it('supports more existing tags', () => {
+    cy.request(`${tagsUrl}tags=tag1&tags=tag2`).as('taggedPets')
 
-  //   cy.get('@taggedPets').then((response: any) => {})
-  // })
+    cy.fixture('findPetByTag/tag1_tag2.json').then((json) => {
+      cy.get('@taggedPets').then((response: any) => {
+        expect(response.status).to.eq(200)
 
-  // it('lists pets tagged with tag0', () => {
-  //   cy.request(`${tagsUrl}tags=tag0`).as('taggedPets')
+        expect(response.body).to.deep.equal(json)
+      })
+    })
+  })
 
-  //   cy.get('@taggedPets').then((response: any) => {})
-  // })
+  it('returns an empty array for inexisting tags', () => {
+    cy.request(`${tagsUrl}tags=tag0`).as('taggedPets0')
+    cy.request(`${tagsUrl}tags=tag5`).as('taggedPets5')
+    cy.request(`${tagsUrl}tags=thiswillneverexist`).as('thiswillneverexist')
 
-  // it('lists pets tagged with tag5', () => {
-  //   cy.request(`${tagsUrl}tags=tag5`).as('taggedPets')
+    const requests = ['@taggedPets0', '@taggedPets5', '@thiswillneverexist']
 
-  //   cy.get('@taggedPets').then((response: any) => {})
-  // })
+    requests.forEach(alias => {
+      cy.get(alias).then((response: any) => {
+        expect(response.status).to.eq(200)
 
-  // it('lists pets tagged with thiswillneverexist', () => {
-  //   cy.request(`${tagsUrl}tags=thiswillneverexist`).as('taggedPets')
+        expect(response.body).to.deep.equal([])
+      })
+    });
+  })
 
-  //   cy.get('@taggedPets').then((response: any) => {})
-  // })
+  it('returns existing tags and ignores inexsting ones', () => {
+    cy.request(`${tagsUrl}tags=tag1&tags=thiswillneverexist`).as('taggedPets')
 
-  // it('lists pets tagged with tag1 and thiswillneverexist', () => {
-  //   cy.request(`${tagsUrl}tags=tag1&tags=thiswillneverexist`).as('taggedPets')
-
-  //   cy.get('@taggedPets').then((response: any) => {})
-  // })
+    cy.get('@taggedPets').then((response: any) => { })
+  })
 })
