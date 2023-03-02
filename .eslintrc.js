d1ac7f82fcc46a9a5884d1af0920418e91e3645a
Alex Radulescu <allex.radulescu91@gmail.com>
2023-03-02 14:07:06 +0100

    feat: add cypress linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['prettier', 'plugin:import/recommended'],
+  extends: [
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:cypress/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
